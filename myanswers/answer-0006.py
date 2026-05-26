ceeff6f6d51db08b6f446fd5135223ca6350f7a4
Santiago Castaño Pérez <castanoperezsantiago@gmail.com>
2026-05-26 08:53:47 -0500

    Update answer-0006.py

--- myanswers/answer-0006.py
+++ myanswers/answer-0006.py
@@ -1,1 +1,6 @@
-
+def resumir_interferencias(df):
+    return (
+        df.groupby('nivel_edificio')[['id_clash']]
+        .count()
+        .rename(columns={'id_clash': 'total_conflictos'})
+    )
